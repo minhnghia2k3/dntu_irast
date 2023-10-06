44e55693a6dcedd69b42a1cfcd868f36598793c6
minhnghia2k3 <BrozennVN@gmail.com>
2023-10-06 21:03:11 +0700

    Create route checking

---
+++ client/dntu-irast/app/test/[...video]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Page() {
+    return (
+        <div>Page video</div>
+    )
+}
+
+export default Page
---
+++ client/dntu-irast/app/test/[companyId]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>Company id</div>
+    )
+}
+
+export default page
