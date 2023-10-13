d30e52b0336450eb74d98ff017c584575399dea2
minhnghia2k3 <BrozennVN@gmail.com>
2023-10-13 23:40:34 +0700

    Doing add form

---
+++ client/dntu-irast/utils/ApiRoutes.js
@@ -0,0 +1,9 @@
+export const HOST = "http://localhost:8080";
+
+export const COMPANY_API = `${HOST}/api/companies`;
+export const UPLOADS_API = `${HOST}/api/uploads`;
+
+export const GET_ALL_COMPANIES_ROUTE = `${COMPANY_API}/companies`;
+export const CREATE_COMPANY_ROUTE = `${COMPANY_API}/create-company`;
+export const UPDATE_COMPANY_ROUTE = `${COMPANY_API}/update-company`;
+export const DELETE_COMPANY_ROUTE = `${COMPANY_API}/delete-company`;
